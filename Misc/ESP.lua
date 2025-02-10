6ed369acdb24de5efd7631b08c72c0832d3c7411
auyu <73044953+omwfh@users.noreply.github.com>
2025-02-09 23:15:42 -0800

    Update ESP.lua

--- Misc/ESP.lua
+++ Misc/ESP.lua
@@ -1,30 +1,35 @@
 local FriendLocator = loadstring(game:HttpGet("https://raw.githubusercontent.com/omwfh/seo/refs/heads/main/Packages/Modules/FriendLocator.lua"))()
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local UserInputService = game:GetService("UserInputService")
+local Players: Players = game:GetService("Players")
+local RunService: RunService = game:GetService("RunService")
+local UserInputService: UserInputService = game:GetService("UserInputService")
+local LocalPlayer: Player = Players.LocalPlayer
 
-local activeLocators = {}
+local activeLocators: { [number]: FriendLocator } = {}
 
-local function checkForFriends()
-    if not getgenv().FriendLocatorEnabled then return end
-
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player ~= Players.LocalPlayer and Players.LocalPlayer:IsFriendsWith(player.UserId) then
-            if not activeLocators[player.UserId] then
-                local locator = FriendLocator.new(player.UserId)
-                activeLocators[player.UserId] = locator
-            end
+local function addFriendLocator(player: Player)
+    if player ~= LocalPlayer and LocalPlayer:IsFriendsWith(player.UserId) then
+        if not activeLocators[player.UserId] then
+            local locator = FriendLocator.new(player.UserId)
+            activeLocators[player.UserId] = locator
         end
     end
 end
 
-local function removeFriendLocator(player)
+local function removeFriendLocator(player: Player)
     if activeLocators[player.UserId] then
         activeLocators[player.UserId]:Destroy()
         activeLocators[player.UserId] = nil
     end
 end
 
-RunService.Heartbeat:Connect(checkForFriends)
+local function checkForFriends()
+    if not getgenv().FriendLocatorEnabled then return end
+    for _, player in ipairs(Players:GetPlayers()) do
+        addFriendLocator(player)
+    end
+end
+
+Players.PlayerAdded:Connect(addFriendLocator)
 Players.PlayerRemoving:Connect(removeFriendLocator)
+RunService.Heartbeat:Connect(checkForFriends)

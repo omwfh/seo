ffffb40a56085db4741afd8447021f9b02412145
auyu <73044953+omwfh@users.noreply.github.com>
2025-02-26 16:28:36 -0500

    Update PingUI.lua

--- Misc/PingUI.lua
+++ Misc/PingUI.lua
@@ -7,10 +7,10 @@ local LocalPlayer: Player = Players.LocalPlayer
 local newInstance: (className: string) -> Instance = Instance.new
 local lastPing: number = -1
 local uiConnection: RBXScriptConnection?
+local ScreenGui: ScreenGui?
 
 local function protectScreenGui(screenGui: ScreenGui)
     assert(screenGui and screenGui:IsA("ScreenGui"), "[SEO] Invalid argument: screenGui must be a valid ScreenGui instance.")
-
     task.defer(function()
         if syn and syn.protect_gui then
             syn.protect_gui(screenGui)
@@ -39,11 +39,12 @@ local function getPlayerPing(): number
 end
 
 local function createPingUI()
-    if uiConnection then
-        uiConnection:Disconnect()
+    if ScreenGui then
+        ScreenGui:Destroy()
+        ScreenGui = nil
     end
 
-    local ScreenGui: ScreenGui = newInstance("ScreenGui")
+    ScreenGui = newInstance("ScreenGui")
     ScreenGui.Name = "PingDisplay"
     protectScreenGui(ScreenGui)
 
@@ -52,52 +53,33 @@ local function createPingUI()
     ClientInfo.Parent = ScreenGui
     ClientInfo.Position = UDim2.new(0.005, 0, 1, -54)
     ClientInfo.Size = UDim2.new(0, 119, 0, 54)
-    ClientInfo.SizeConstraint = Enum.SizeConstraint.RelativeXY
-    ClientInfo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     ClientInfo.BackgroundTransparency = 1
-    ClientInfo.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    ClientInfo.BorderMode = Enum.BorderMode.Outline
-    ClientInfo.BorderSizePixel = 0
-    ClientInfo.Visible = true
-    ClientInfo.ZIndex = 1
-
-    local UISizeConstraint: UISizeConstraint = newInstance("UISizeConstraint")
-    UISizeConstraint.Parent = ClientInfo
-    UISizeConstraint.MaxSize = Vector2.new(math.huge, math.huge)
-    UISizeConstraint.MinSize = Vector2.new(0, 0)
 
     local TextLabel: TextLabel = newInstance("TextLabel")
     TextLabel.Parent = ClientInfo
     TextLabel.Name = "PingLabel"
-    TextLabel.Position = UDim2.new(-0.008, 0, 0.572, 0)
+    TextLabel.Position = UDim2.new(0, 0, 0.572, 0)
     TextLabel.Size = UDim2.new(0, 200, 0, 14)
     TextLabel.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Bold)
     TextLabel.TextSize = 14
     TextLabel.TextTransparency = 0.5
     TextLabel.TextColor3 = Color3.new(1, 1, 1)
     TextLabel.BackgroundTransparency = 1
-    TextLabel.TextWrapped = true
     TextLabel.TextXAlignment = Enum.TextXAlignment.Left
     TextLabel.TextYAlignment = Enum.TextYAlignment.Center
-
-    uiConnection = RunService.Heartbeat:Connect(function()
-        local ping: number = getPlayerPing()
-        if ping ~= lastPing and ping ~= -1 then
-            lastPing = ping
-            TextLabel.Text = `Ping: {ping} ms`
-        end
-    end)
-
-    LocalPlayer.CharacterRemoving:Connect(function()
-        if ScreenGui then
-            ScreenGui:Destroy()
-        end
-    end)
-
-    LocalPlayer.CharacterAdded:Connect(function()
-        task.wait(0.15)
-        createPingUI()
-    end)
 end
+
+uiConnection = RunService.Heartbeat:Connect(function()
+    local ping: number = getPlayerPing()
+    if ping ~= lastPing and ping ~= -1 then
+        lastPing = ping
+            if ScreenGui then
+            local TextLabel = ScreenGui:FindFirstChild("ClientInfo"):FindFirstChild("PingLabel")
+                if TextLabel then
+                TextLabel.Text = `Ping: {ping} ms`
+            end
+        end
+    end
+end)
 
 createPingUI()

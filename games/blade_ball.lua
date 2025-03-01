8554a29f482545a99893be3e97d7b071870f9e3b
auyu <73044953+omwfh@users.noreply.github.com>
2025-03-01 13:00:41 -0800

    Update blade_ball.lua

--- games/blade_ball.lua
+++ games/blade_ball.lua
@@ -22,9 +22,9 @@ local configHighPing: { value1: number, value2: number, value3: number, value4: 
 
 local configLowPing: { value1: number, value2: number, value3: number, value4: number } = {
 	value1 = 0.107,
-	value2 = 0.0056,
-	value3 = 0.01,
-	value4 = 0.31
+	value2 = 0.006,
+	value3 = 0.013,
+	value4 = 0.27
 }
 
 local currentConfig = nil

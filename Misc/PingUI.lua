c8342cd53bfb0f3e7beae954a99e66509682e8f9
auyu <73044953+omwfh@users.noreply.github.com>
2025-03-03 01:10:45 -0800

    Update PingUI.lua

--- Misc/PingUI.lua
+++ Misc/PingUI.lua
@@ -1,86 +1,85 @@
-local Players = game:GetService("Players")
-local UserInputService = game:GetService("UserInputService")
+local Players: Players = game:GetService("Players")
+local Stats: Stats = game:GetService("Stats")
+local RunService: RunService = game:GetService("RunService")
+local CoreGui: CoreGui = game:GetService("CoreGui")
 
-local v1: Player = Players.LocalPlayer
-local v4: number = 0.058
-local v5: Enum.UserInputType = Enum.UserInputType.MouseButton2
-local v7: boolean = false
-
+local LocalPlayer: Player = Players.LocalPlayer
+local newInstance: (className: string) -> Instance = Instance.new
+local lastPing: number = -1
+local uiConnection: RBXScriptConnection?
 local ScreenGui: ScreenGui?
-local InputBox: TextBox?
 
 local function protectScreenGui(screenGui: ScreenGui)
     assert(screenGui and screenGui:IsA("ScreenGui"), "[SEO] Invalid argument: screenGui must be a valid ScreenGui instance.")
     task.defer(function()
         if syn and syn.protect_gui then
             syn.protect_gui(screenGui)
-            screenGui.Parent = game:GetService("CoreGui")
+            screenGui.Parent = CoreGui
         elseif gethui then
             screenGui.Parent = gethui()
         else
-            screenGui.Parent = game:GetService("CoreGui")
+            screenGui.Parent = CoreGui
         end
     end)
 end
 
-local function Initiate()
+local function getPlayerPing(): number
+    local NetworkStats: Folder? = Stats:FindFirstChild("Network")
+    
+    if NetworkStats then
+        local PingItem: Instance? = NetworkStats:FindFirstChild("ServerStatsItem")
+        local PingValue: number? = PingItem and PingItem:FindFirstChild("Data Ping") and PingItem["Data Ping"]:GetValue()
+
+        if typeof(PingValue) == "number" then
+            return math.clamp(math.round(PingValue), 0, 1000)
+        end
+    end
+
+    return -1
+end
+
+local function createPingUI()
     if ScreenGui then
         ScreenGui:Destroy()
         ScreenGui = nil
     end
 
-    ScreenGui = Instance.new("ScreenGui")
-    ScreenGui.Name = "SmoothingInput"
+    ScreenGui = newInstance("ScreenGui")
+    ScreenGui.Name = "PingDisplay"
     protectScreenGui(ScreenGui)
 
-    local InputFrame: Frame = Instance.new("Frame")
-    InputFrame.Name = "Values"
-    InputFrame.Parent = ScreenGui
-    InputFrame.Position = UDim2.new(0.005, 0, 1, -64)
-    InputFrame.Size = UDim2.new(0, 119, 0, 30)
-    InputFrame.BackgroundTransparency = 1
+    local ClientInfo: Frame = newInstance("Frame")
+    ClientInfo.Name = "ClientInfo"
+    ClientInfo.Parent = ScreenGui
+    ClientInfo.Position = UDim2.new(0.005, 0, 1, -54)
+    ClientInfo.Size = UDim2.new(0, 119, 0, 54)
+    ClientInfo.BackgroundTransparency = 1
 
-    InputBox = Instance.new("TextBox")
-    InputBox.Parent = InputFrame
-    InputBox.Name = "ValueInput"
-    InputBox.Position = UDim2.new(0, 0, 0.572, 0)
-    InputBox.Size = UDim2.new(0, 119, 0, 20)
-    InputBox.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Bold)
-    InputBox.TextSize = 14
-    InputBox.TextTransparency = 0.2
-    InputBox.TextColor3 = Color3.new(1, 1, 1)
-    InputBox.BackgroundTransparency = 0.4
-    InputBox.BackgroundColor3 = Color3.new(0, 0, 0)
-    InputBox.TextXAlignment = Enum.TextXAlignment.Center
-    InputBox.TextYAlignment = Enum.TextYAlignment.Center
-    InputBox.Text = tostring(v4)
-    InputBox.ClearTextOnFocus = false
-
-    InputBox.FocusLost:Connect(function(enterPressed)
-        if enterPressed then
-            local newValue = tonumber(InputBox.Text)
-            if newValue then
-                v4 = math.clamp(newValue, 0.01, 0.2)
-                print("v4 value updated to:", v4)
-            else
-                InputBox.Text = tostring(v4)
-            end
-        end
-    end)
-    
-    UserInputService.InputBegan:Connect(function(input, gameProcessed)
-        if gameProcessed then return end
-        if input.UserInputType == v5 and not v7 then
-            v7 = true
-        end
-    end)
-
-    UserInputService.InputEnded:Connect(function(input)
-        if input.UserInputType == v5 then
-            v7 = false
-            print("Smooth Aim Stopped")
-        end
-    end)
+    local TextLabel: TextLabel = newInstance("TextLabel")
+    TextLabel.Parent = ClientInfo
+    TextLabel.Name = "PingLabel"
+    TextLabel.Position = UDim2.new(0, 0, 0.572, 0)
+    TextLabel.Size = UDim2.new(0, 200, 0, 14)
+    TextLabel.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Bold)
+    TextLabel.TextSize = 14
+    TextLabel.TextTransparency = 0.5
+    TextLabel.TextColor3 = Color3.new(1, 1, 1)
+    TextLabel.BackgroundTransparency = 1
+    TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+    TextLabel.TextYAlignment = Enum.TextYAlignment.Center
 end
 
-Initiate()
+uiConnection = RunService.Heartbeat:Connect(function()
+    local ping: number = getPlayerPing()
+    if ping ~= lastPing and ping ~= -1 then
+        lastPing = ping
+            if ScreenGui then
+            local TextLabel = ScreenGui:FindFirstChild("ClientInfo"):FindFirstChild("PingLabel")
+                if TextLabel then
+                TextLabel.Text = `Ping: {ping} ms`
+            end
+        end
+    end
+end)
+
+createPingUI()

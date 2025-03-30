97bf05530522bbc882b2f10d0f69a97f88601d72
auyu <73044953+omwfh@users.noreply.github.com>
2025-03-29 20:31:03 -0700

    Update blade_ball.lua

--- games/blade_ball.lua
+++ games/blade_ball.lua
@@ -14,16 +14,16 @@ local lastPressTime: { [Instance]: number } = {}
 local isKeyPressed: { [Instance]: boolean } = {}
 
 local configHighPing: { value1: number, value2: number, value3: number, value4: number } = {
-    value1 = 0.104,
-    value2 = 0.0065,
+    value1 = 0.106,
+    value2 = 0.0063,
     value3 = 0.0106,
     value4 = 0.31
 }
 
 local configLowPing: { value1: number, value2: number, value3: number, value4: number } = {
-    value1 = 0.107,
+    value1 = 0.105,
     value2 = 0.0057,
-    value3 = 0.011,
+    value3 = 0.0109,
     value4 = 0.27
 }
 
